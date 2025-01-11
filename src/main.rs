e55191be4aff2bf1d0b191798c1ceea3b63583c5
Donte Littlejohn <donteravae@gmail.com>
2025-01-11 16:06:24 -0500

    refined app configuration and split into environment based settings

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,21 @@ use zero2prod::{
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Handle telemetry
     let subscriber = get_subscriber("zero2prod", "info", std::io::stdout);
     init_subscriber(subscriber);
 
+    // Get configuration settings for database and application host address
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres");
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
+            .expect("Failed to create Postgres connection pool.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    // Listen at address and run app
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
